c6f5e84b205299d1c5dfb0c9815c859d2193511a
vladahvala <80634246+vladahvala@users.noreply.github.com>
2025-01-25 23:38:33 +0200

    Search bar added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
 /*=============== SEARCH ===============*/
+/*=============== SHOW MENU ===============*/
+const searchButton = document.getElementById('search-button'),
+      searchClose = document.getElementById('search-close'),
+      searchContent = document.getElementById('search-content')
 
+/* Menu show */
+if(searchButton){
+    searchButton.addEventListener('click', () =>{
+        searchContent.classList.add('show-search')
+    })
+}
+
+/* Menu hidden */
+if(searchClose){
+    searchClose.addEventListener('click', () =>{
+        searchContent.classList.remove('show-search')
+    })
+}
 
 /*=============== LOGIN ===============*/
 
